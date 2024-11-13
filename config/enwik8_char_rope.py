254e9852cb7666d11cb58b1278cdd7dd15c26971
Mikhail <tsitsvero@gmail.com>
2024-11-13 12:44:55 +0900

    510

--- config/enwik8_char_rope.py
+++ config/enwik8_char_rope.py
@@ -19,7 +19,7 @@ block_size = 256  # Reduced context length
 # Model parameters
 n_layer = 12
 n_head = 6
-n_embd = 512
+n_embd = 510
 dropout = 0.1  # Removed dropout initially for debugging
 bias = False
 
